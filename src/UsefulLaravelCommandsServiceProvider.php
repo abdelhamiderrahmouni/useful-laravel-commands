463cfdd64c415617f3b2f63c0c4f0682820b231b
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2025-06-25 13:04:16 +0100

    remove unused imports from UsefulLaravelCommandsServiceProvider.php

--- src/UsefulLaravelCommandsServiceProvider.php
+++ src/UsefulLaravelCommandsServiceProvider.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace UsefulLaravelCommands;
 
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use UsefulLaravelCommands\Console\Commands\ResetUserPassword;
 
